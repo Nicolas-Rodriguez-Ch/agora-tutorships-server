68701ed56adf45e9d63d1589145c15d3594f5092
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-05-18 18:34:33 -0500

    format

--- controllers/tutorSearch.controller.js
+++ controllers/tutorSearch.controller.js
@@ -1,25 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Tutor = require("../models/tutor.model");
-
 
 const findTutors = async (req, res) => {
   try {
-    const page = req.params.page || 1
-    const count = await Tutor.count({focus: req.params.focus})
-    const data = await Tutor.find({focus: req.params.focus},
-      ['name', 'profile_photo', 'profession', 'focus', 'rating', 'description', 'price'])
-      .sort({rating: -1})
-      .skip((page-1)*9)
-      .limit(9)
- 
+    const page = req.params.page || 1;
+    const count = await Tutor.count({ focus: req.params.focus });
+    const data = await Tutor.find({ focus: req.params.focus }, [
+      "name",
+      "profile_photo",
+      "profession",
+      "focus",
+      "rating",
+      "description",
+      "price",
+    ])
+      .sort({ rating: -1 })
+      .skip((page - 1) * 9)
+      .limit(9);
+
     res.json({
       count,
       page,
-      data
+      data,
     });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
 };
 
-module.exports =  {findTutors};
+module.exports = { findTutors };
