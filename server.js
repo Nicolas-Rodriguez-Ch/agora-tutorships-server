0c224e8417443009d6de42c8cd3e29fb57b304d2
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-05-27 13:35:33 -0500

    starts the implementation of chat and web sockets

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-const app = require('./app');
+const app = require("./app");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["POST", "GET"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
