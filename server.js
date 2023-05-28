d6b19daa3b53afaf315061314315ec451df15d1c
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-05-28 11:34:08 -0500

    gets chat working

--- server.js
+++ server.js
@@ -7,13 +7,37 @@ const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONT_END_API || "http://localhost:3000",
     method: ["POST", "GET"],
   },
 });
 
+let privateChats = new Map();
+
 io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.emit("welcome", { message: "Welcome to chat" });
+
+  socket.on("join room", (data) => {
+    console.log("🚀 ~ file: server.js:22 ~ socket.on ~ data:", data)
+    console.log(" someone joined to room; " + data.roomId);
+    if (!privateChats.has(data.roomId)) {
+      privateChats.set(data.roomId, [data.userId]);
+    } else {
+      const users = privateChats.get(data.roomId);
+      if (!users.includes(data.userId)) {
+        users.push(data.userId);
+        privateChats.set(data.roomId, users);
+      }
+    }
+    socket.join(data.roomId);
+  });
+
+  socket.on("message", (data) => {
+    if (privateChats.get(data.roomId).includes(data.userId)) {
+      io.to(data.roomId).emit("new message", data.message);
+    }
+  });
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
